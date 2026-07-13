0928695b941b654db82dce8b5dad2512845266a1
Matusalen Alves <matusalencosta@icloud.com>
2026-07-13 11:38:07 -0300

    ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -21,9 +21,13 @@ public class ChessMatch {
         return matrix;
     }
 
+    public void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
     public void initialSetup() {
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-        board.placePiece(new Rook(board, Color.WHITE), new Position(0, 4));
-        board.placePiece(new King(board, Color.BLACK), new Position(7, 4));
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new King(board, Color.BLACK));
     }
 }
