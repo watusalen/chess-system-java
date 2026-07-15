56c1bb9706cb57b3f069631c5f5f5539b3b98a6d
Matusalen Alves <matusalencosta@icloud.com>
2026-07-15 12:35:32 -0300

    Handling captured pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,17 +5,21 @@ import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         ChessMatch match = new ChessMatch();
         Scanner scanner = new Scanner(System.in);
+        List<ChessPiece> captured = new ArrayList<>();
+
         while (true) {
             try {
                 UI.clearScreen();
-                UI.printBoard(match.getPieces());
+                UI.printMatch(match, captured);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(scanner);
@@ -29,6 +33,9 @@ public class Program {
                 ChessPosition target = UI.readChessPosition(scanner);
 
                 ChessPiece capturedPiece = match.performChessMove(source, target);
+                if (capturedPiece != null) {
+                    captured.add(capturedPiece);
+                }
             } catch (ChessException e) {
                 System.out.print(e.getMessage());
                 scanner.nextLine();
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -4,23 +4,23 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChessMatch {
-    private Board board;
+    private final Board board;
     private int turn;
     private Color currentPlayer;
 
-    public int getTurn() {
-        return turn;
-    }
-
-    public Color getCurrentPlayer() {
-        return currentPlayer;
-    }
+    private List<ChessPiece> piecesOnTheBoard;
+    private List<ChessPiece> capturedPieces;
 
     public ChessMatch() {
         board = new Board(8, 8);
         turn = 1;
         currentPlayer = Color.WHITE;
+        piecesOnTheBoard = new ArrayList<>();
+        capturedPieces = new ArrayList<>();
         initialSetup();
     }
 
@@ -32,6 +32,14 @@ public class ChessMatch {
             }
         }
         return matrix;
+    }
+
+    public int getTurn() {
+        return turn;
+    }
+
+    public Color getCurrentPlayer() {
+        return currentPlayer;
     }
 
     public boolean[][] possibleMoves(ChessPosition sourcePosition) {
@@ -54,6 +62,10 @@ public class ChessMatch {
         Piece piece = board.removePiece(source);
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(piece, target);
+        if (capturedPiece != null) {
+            piecesOnTheBoard.remove((ChessPiece) capturedPiece);
+            capturedPieces.add((ChessPiece) capturedPiece);
+        }
         return capturedPiece;
     }
 
@@ -61,7 +73,7 @@ public class ChessMatch {
         if (!board.thereIsAPiece(position)) {
             throw new ChessException("There is no piece on source position.");
         }
-        if(currentPlayer != ((ChessPiece)board.piece(position)).getColor()){
+        if (currentPlayer != ((ChessPiece) board.piece(position)).getColor()) {
             throw new ChessException("The chosen piece isn't yours.");
         }
         if (!board.piece(position).isThereAnyPossibleMove()) {
@@ -75,13 +87,14 @@ public class ChessMatch {
         }
     }
 
-    private void nextTurn(){
+    private void nextTurn() {
         turn++;
         currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
     }
 
     public void placeNewPiece(char column, int row, ChessPiece piece) {
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
+        piecesOnTheBoard.add(piece);
     }
 
     public void initialSetup() {
